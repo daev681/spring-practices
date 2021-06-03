4ab1d36db37867bd3ff2b5e5a6c54f9e82660536
wow1186 <WOW1186@NAVER.COM>
2021-06-03 15:13:32 +0900

    guestbook03 add

--- hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -33,6 +33,7 @@ public class UserController {
 	@RequestMapping(value = "/join" , method = RequestMethod.POST)
 	public String join(@ModelAttribute UserVo vo) { // UserVo vo Model model
 		// model.addAttribute("userVo, vo");
+	
 		System.out.println(vo);
 		return "redirect:/";
 	}
